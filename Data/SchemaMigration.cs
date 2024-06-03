8c02f84a2949e8828dbcdaf723ae8a7fc3e7ed05
Mehmet Kurt <mehmet.kurt@outlook.com>
2024-06-03 09:48:20 +0300

    refactor: Change SchemaMigration to use AutoReversingMigration
    - Updated SchemaMigration class to use AutoReversingMigration instead of ForwardOnlyMigration
    - Ensured that the migration automatically handles both installation and uninstallation of the schema

--- Data/SchemaMigration.cs
+++ Data/SchemaMigration.cs
@@ -6,7 +6,7 @@ using Nop.Plugin.Misc.Elasticsearch.Data.Domain;
 namespace Nop.Plugin.Misc.Elasticsearch.Data;
 
 [NopSchemaMigration("2023/06/03 07:20:00:1453071", "Nop.Plugin.Misc.Elasticsearch base schema", MigrationProcessType.Installation)]
-public class SchemaMigration : ForwardOnlyMigration
+public class SchemaMigration : AutoReversingMigration
 {
     public override void Up()
     {
