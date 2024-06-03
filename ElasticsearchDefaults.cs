ad3ad17b06f03e4776c0edd27ce9ebcd2f9f3f6a
Mehmet Kurt <mehmet.kurt@outlook.com>
2024-06-03 08:08:40 +0300

    feat: Add summaries for variables in ElasticsearchDefaults class
    - Added XML summaries to describe the purpose of each constant in ElasticsearchDefaults class

--- ElasticsearchDefaults.cs
+++ ElasticsearchDefaults.cs
@@ -1,7 +1,20 @@
-﻿namespace Nop.Plugin.Misc.Elasticsearch;
+﻿/// <summary>
+/// Contains default values and constants for the Elasticsearch plugin.
+/// </summary>
 public static partial class ElasticsearchDefaults
 {
+    /// <summary>
+    /// Gets or sets the system name of the Elasticsearch plugin.
+    /// </summary>
     public const string PluginSystemName = "Misc.Elasticsearch";
+
+    /// <summary>
+    /// Gets or sets the localization prefix for the Elasticsearch plugin.
+    /// </summary>
     public const string LocalizationPrefix = "Nop.Plugin.Misc.Elasticsearch";
+
+    /// <summary>
+    /// Gets or sets the system name of the Elasticsearch plugin's admin menu.
+    /// </summary>
     public const string AdminMenuSystemName = "Elasticsearch Configuration";
 }
