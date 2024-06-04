4339fe3b73902b668957e5684bc7081f9e3e028e
Mehmet Kurt <mehmet.kurt@outlook.com>
2024-06-04 17:34:38 +0300

    docs: Add XML comments to OperationType enum and EntityTransfer class
    Improved code readability with XML comments for enum values and class properties.
    
    Tags: #documentation #codequality

--- Data/Domain/EntityTransfer.cs
+++ Data/Domain/EntityTransfer.cs
@@ -1,15 +1,45 @@
 ﻿using Nop.Core;
 
 namespace Nop.Plugin.SearchProvider.Elasticsearch.Data.Domain;
+
+/// <summary>
+/// Represents an entity transfer record.
+/// </summary>
 public class EntityTransfer : BaseEntity
 {
+    /// <summary>
+    /// Gets or sets the name of the entity.
+    /// </summary>
     public string EntityName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the identifier of the entity.
+    /// </summary>
     public int EntityId { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the transfer is ignored.
+    /// </summary>
     public bool Ignored { get; set; }
+
+    /// <summary>
+    /// Gets or sets the identifier of the operation type.
+    /// </summary>
     public int OperationTypeId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date and time when the transfer was created (in UTC).
+    /// </summary>
     public DateTime CreatedDateUtc { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date and time when the transfer was last updated (in UTC).
+    /// </summary>
     public DateTime UpdatedDateUtc { get; set; }
 
+    /// <summary>
+    /// Gets or sets the operation type.
+    /// </summary>
     public OperationType OperationType
     {
         get => (OperationType)OperationTypeId;
--- Data/Domain/OperationType.cs
+++ Data/Domain/OperationType.cs
@@ -1,7 +1,22 @@
 ﻿namespace Nop.Plugin.SearchProvider.Elasticsearch.Data.Domain;
+
+/// <summary>
+/// Represents the type of operation performed on an entity.
+/// </summary>
 public enum OperationType
 {
+    /// <summary>
+    /// Indicates that an entity has been inserted.
+    /// </summary>
     Inserted = 10,
+
+    /// <summary>
+    /// Indicates that an entity has been updated.
+    /// </summary>
     Updated = 20,
+
+    /// <summary>
+    /// Indicates that an entity has been deleted.
+    /// </summary>
     Deleted = 30
 }
