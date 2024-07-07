49b005c53a9c7f94f340eb05112be8cbc499c7ec
Mehmet Kurt <mehmet.kurt@outlook.com>
2024-07-07 12:40:13 +0300

    docs: added missing summary comments and fixed inaccuracies

--- Consumers/BaseEntityConsumer.cs
+++ Consumers/BaseEntityConsumer.cs
@@ -6,6 +6,10 @@ using Nop.Services.Events;
 
 namespace Nop.Plugin.SearchProvider.Elasticsearch.Consumers;
 
+/// <summary>
+/// Represents a base entity consumer that handles entity events such as insertion, update, and deletion.
+/// </summary>
+/// <typeparam name="TEntity">The type of the entity.</typeparam>
 public abstract partial class BaseEntityConsumer<TEntity> :
     IConsumer<EntityInsertedEvent<TEntity>>,
     IConsumer<EntityUpdatedEvent<TEntity>>,
@@ -17,53 +21,59 @@ public abstract partial class BaseEntityConsumer<TEntity> :
     #endregion
 
     #region Ctor
-    protected BaseEntityConsumer
-    (
-        IEntityTransferService entityTransferService
-    )
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BaseEntityConsumer{TEntity}"/> class.
+    /// </summary>
+    /// <param name="entityTransferService">The entity transfer service.</param>
+    protected BaseEntityConsumer(IEntityTransferService entityTransferService)
     {
         _entityTransferService = entityTransferService;
     }
     #endregion
 
     #region Utilities
+    /// <summary>
+    /// Prepares the entity for transfer based on the operation type.
+    /// </summary>
+    /// <param name="entity">The entity to be transferred.</param>
+    /// <param name="operationType">The type of the operation being performed.</param>
+    /// <returns>A task that represents the asynchronous operation, containing a boolean result indicating if the preparation was successful.</returns>
     protected virtual async Task<bool> PrepareToTransferAsync(TEntity entity, OperationType operationType)
     {
-        // Implementation details here
-        return false;
+        return await Task.FromResult(false);
     }
     #endregion
 
     #region Methods
 
     /// <summary>
-    /// Handle entity inserted event
+    /// Handles the entity inserted event.
     /// </summary>
-    /// <param name="eventMessage">Event message</param>
-    /// <returns>A task that represents the asynchronous operation</returns>
+    /// <param name="eventMessage">The event message containing the entity data.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public virtual async Task HandleEventAsync(EntityInsertedEvent<TEntity> eventMessage)
     {
-        // Implementation details here
+        await Task.CompletedTask;
     }
 
     /// <summary>
-    /// Handle entity updated event
+    /// Handles the entity updated event.
     /// </summary>
-    /// <param name="eventMessage">Event message</param>
-    /// <returns>A task that represents the asynchronous operation</returns>
+    /// <param name="eventMessage">The event message containing the entity data.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public virtual async Task HandleEventAsync(EntityUpdatedEvent<TEntity> eventMessage)
     {
-        // Implementation details here
+        await Task.CompletedTask;
     }
 
     /// <summary>
-    /// Handle entity deleted event
+    /// Handles the entity deleted event.
     /// </summary>
-    /// <param name="eventMessage">Event message</param>
-    /// <returns>A task that represents the asynchronous operation</returns>
+    /// <param name="eventMessage">The event message containing the entity data.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public virtual async Task HandleEventAsync(EntityDeletedEvent<TEntity> eventMessage)
     {
-        // Implementation details here
+        await Task.CompletedTask;
     }
 
     #endregion
